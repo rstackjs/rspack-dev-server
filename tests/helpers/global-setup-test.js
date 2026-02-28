1a678982e2c4253a61a9f9dd445007c98066dafa
9aoy <9aoyuao@gmail.com>
2026-02-28 15:32:10 +0800

    test: remove useless global setup file (#82)

--- tests/helpers/global-setup-test.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const tcpPortUsed = require('tcp-port-used');
-const { webpackVersion } = require('@rspack/core/package.json');
-const ports = require('./ports-map');
-
-// eslint-disable-next-line no-console
-console.log(`\n Running tests for webpack @${webpackVersion} \n`);
